998057763f6510f7ccb7accc6d8c6ee10af0a819
Ralph Berrett <rberrett4@gmail.com>
2021-12-02 19:33:55 -0500

    Day 1 Solution

---
+++ day1/day1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+def load():
+    with open("input.txt") as file:
+        lines = file.readlines()
+        numbers = []
+        for line in lines:
+            numbers.append(int(line))
+        return numbers
+
+def day1_part1(lines):
+    increases = 0
+    last = lines[0]
+    for line in lines[1:]:
+        new = line
+        if new > last: increases += 1
+        last = new
+    print("PART1: %d of %s lines increased" % (increases, len(lines)))
+
+def day1_part2(lines):
+    increases = 0
+    last = sum(lines[0:3])
+
+    for i in range(1, len(lines)):
+        new = sum(lines[i:i+3])
+        if new > last: increases += 1
+        last = new
+    print("PART2: %d of %s lines increased" % (increases, len(lines)))
+
+if __name__ == "__main__":
+    lines = load()
+    day1_part1(lines)
+    day1_part2(lines)
