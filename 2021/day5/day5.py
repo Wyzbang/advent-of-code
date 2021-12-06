a4093ec1522a3da484a9bcd93689ceb3520982fe
Ralph Berrett <rberrett4@gmail.com>
2021-12-05 20:49:22 -0500

    Advent of Code Day 5 Part 2. Refactored Part 1

--- 2021/day5/day5.py
+++ 2021/day5/day5.py
@@ -19,10 +19,12 @@ class Vent:
 
 
 class Grid:
-    def __init__(self, data):
+    def __init__(self, vents, diagonal):
         self.grid = []
-        self.init_grid(data)
-        self.populate_grid(data)
+        self.diagonal = diagonal
+        self.init_grid(vents)
+        self.populate_grid(vents)
+        self.vents = vents
 
     def print(self):
         for row in self.grid:
@@ -47,29 +49,34 @@ class Grid:
 
     def populate_grid(self, vents):
         for vent in vents:
+
             if vent.x1 == vent.x2:
+                #  vertical line
                 column = vent.x1
-                if vent.y1 > vent.y2:
-                    a = vent.y2
-                    b = vent.y1
-                else:
-                    a = vent.y1
-                    b = vent.y2
-
-                for row in range(a, b+1):
+                down = -1 if vent.y1 > vent.y2 else 1
+                for row in range(vent.y1, vent.y2 + down, down):
                     self.grid[row][column] += 1
 
-            if vent.y1 == vent.y2:
+            elif vent.y1 == vent.y2:
+                # horizontal Line
                 row = vent.y1
-                if vent.x1 > vent.x2:
-                    a = vent.x2
-                    b = vent.x1
-                else:
-                    a = vent.x1
-                    b = vent.x2
-
-                for column in range(a, b+1):
+                left = -1 if vent.x1 > vent.x2 else 1
+                for column in range(vent.x1, vent.x2 + left, left):
                     self.grid[row][column] += 1
+
+            elif self.diagonal:
+                # diagonal line (45 degree only)
+                down = -1 if vent.y1 > vent.y2 else 1
+                left = -1 if vent.x1 > vent.x2 else 1
+
+                # Fix end (range is non-inclusive) and direction
+                xs = range(vent.x1, vent.x2 + left, left)
+                ys = range(vent.y1, vent.y2 + down, down)
+
+                for i in range(len(xs)):
+                    x = xs[i]
+                    y = ys[i]
+                    self.grid[y][x] += 1
 
 
 def load():
@@ -84,8 +91,8 @@ def load():
         return vents
 
 
-def part1(data):
-    grid = Grid(data)
+def part(index, data, diagonal):
+    grid = Grid(data, diagonal)
 
     overlap = 0
     for row in grid.grid:
@@ -93,16 +100,12 @@ def part1(data):
             if value >= 2:
                 overlap += 1
 
-    print("PART1: Overlapping points", overlap)
-
-
-def part2(data):
-
-    print("PART2: ")
+    print("PART%d: Overlapping points %d" % (index, overlap))
+    #grid.print()
 
 
 if __name__ == "__main__":
     print("Advent of Code 2021 - Day 5")
     parsed = load()
-    part1(parsed)
-    part2(parsed)
+    part(1, parsed, False)
+    part(2, parsed, True)
