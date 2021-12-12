cd69db75e0efe667bc190f1eb4afb7e6c6caa651
Ralph Berrett <rberrett4@gmail.com>
2021-12-12 15:33:09 -0500

    Advent of Code Day 12 Part 2

--- 2021/day12/day12.py
+++ 2021/day12/day12.py
@@ -30,30 +30,37 @@ class Caves:
             if b != 'end':
                 self.add_connection(b, a)
 
-    def get_sub(self, path):
+    def get_sub(self, path, max):
         for cave in self.connections[path[-1]]:
             new_path = copy.copy(path)
             new_path.append(cave)
             if cave == 'end':
-                self.paths.append(new_path)
-            elif cave.islower() and cave in path:
+                if new_path not in self.paths:
+                    self.paths.append(new_path)
+                    print("Found %d..." % (len(self.paths)), end="\r")
+            elif cave in ['start', 'end']:
+                pass
+            elif cave.islower() and path.count(cave) > max:
                 pass
             else:
-                self.get_sub(new_path)
+                if cave.islower() and new_path.count(cave) == 2:
+                    self.get_sub(new_path, 0)
+                else:
+                    self.get_sub(new_path, max)
 
-    def get_paths(self):
+    def get_paths(self, max):
         self.paths = []
-        self.get_sub(["start"])
+        self.get_sub(["start"], max)
         return self.paths
 
 
 def run():
     print("Advent of Code 2021 - Day 12")
     caves = Caves('input.txt')
-    paths1 = caves.get_paths()
-    paths2 = []
-    print("PART1: ", len(paths1))
-    print("PART2: ", len(paths2))
+    paths1 = caves.get_paths(0)
+    print("PART1: Single small cave, paths: ", len(paths1))
+    paths2 = caves.get_paths(1)
+    print("PART2: Double first small cave, paths: ", len(paths2))
 
 
 if __name__ == "__main__":
